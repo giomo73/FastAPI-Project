b48f35ff96821765ec8493be778413c79b79f7ad
giomo73 <elimarcleo@gmail.com>
2025-05-11 14:51:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://sts-orebro.onrender.com"
+        "https://fastapi-project-1-qd8d.onrender.com/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
