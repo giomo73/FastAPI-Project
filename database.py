2dc08b18fae365a234c90b522430adda69f448cf
giomo73 <elimarcleo@gmail.com>
2025-05-11 14:19:52 +0200

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,15 @@ from sqlalchemy.orm import sessionmaker
 import os
 
 # Percorso compatibile con ambienti come Render
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/verkstad.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://verkstad_db_user:2d3z9ykneIOHmIHK0SeFp6fYYwGeBxvz@dpg-d0g6d4adbo4c73b2hnig-a.frankfurt-postgres.render.com/verkstad_db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
