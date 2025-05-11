d711cc019ba38daf4b8fc06a2e48c96e47d312d6
giomo73 <elimarcleo@gmail.com>
2025-05-11 14:44:50 +0200

    Delete schemas/hashing.py

--- schemas/hashing.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["argon2", "bcrypt"], deprecated="auto")
-
-def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
-
-def verify_password_argon2(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
